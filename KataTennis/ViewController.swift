1e59aa1664136a49e372912347737f54653bf6d9
Guanshan Liu <guanshan.liu@gmail.com>
2014-11-04 16:10:09 +0800

    Initial Commit

---
+++ KataTennis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KataTennis
+//
+//  Created by Guanshan Liu on 04/11/2014.
+//  Copyright (c) 2014 guanshanliu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
